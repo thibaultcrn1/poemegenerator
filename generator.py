cf00ebdea9bc939171bc02528e63bb92136d276a
KhralidBLK <69095689+thibaultcrn1@users.noreply.github.com>
2020-11-26 21:08:03 +0100

    Update generator.py
    Optimization

--- generator.py
+++ generator.py
@@ -1,35 +1,49 @@
+# Import de la fonction random dans le programme.
 import random
 
-print("-------------------------")
-print("-- Générateur de poème --")
-print("-------------------------")
-print("")
-
-response = float(input("Quel type de phrase ? Syntaxe: 4444 ou 4433 "))
+# Affichage du design au lancement du programme.
+print("================[FR]================")
+print("==         Générateur de          ==")
+print("==            Poème               ==")
+print("====================================")
 print()
 
-list = [
-  "test1",
-  "test2",
-  "test3",
-  "test4"
+# Zone d'écriture pour l'intéraction avec le programme.
+response = float(input("Que voulez-vous comme poème ? Syntaxe: 4444 ou 4433"))
+print()
+
+# Création de la liste de vers pour les poèmes aléatoire.
+list1 = [
+    "test1",
+    "test2",
+    "test3",
+    "test4"
 ]
 
+# Condition qui détecte si le résultat écrit dans la zone d'écriture ci-dessus est égal à 4444
 if response == 4444:
-  print('\n'.join(random.sample(list, 4)))
-  print()
-  print('\n'.join(random.sample(list, 4)))
-  print()
-  print('\n'.join(random.sample(list, 4)))
-  print()
-  print('\n'.join(random.sample(list, 4)))
-  
+    # permet l'affichage en ligne + le mélange avec la fonction random de la liste ci dessus.
+    print('\n'.join(random.sample(list1, 4)))
+    # permet de faire un espace entre chaque strophe.
+    print()
+    print('\n'.join(random.sample(list1, 4)))
+    print()
+    print('\n'.join(random.sample(list1, 4)))
+    print()
+    print('\n'.join(random.sample(list1, 4)))
+
+# Condition qui permet d'afficher un message d'erreur si un autre nombre que 4444 ou 4433 est écrit dans la zone
+# d'intéraction.
+else:
+    print("[ERREUR] Vous avez écrit des caractères qui ne sont pas valide !")
+
+# Condition qui détecte si le résultat écrit dans la zone d'écriture ci-dessus est égal à 4433
 if response == 4433:
-  print('\n'.join(random.sample(list, 4)))
-  print()
-  print('\n'.join(random.sample(list, 4)))
-  print()
-  print('\n'.join(random.sample(list, 3)))
-  print()
-  print('\n'.join(random.sample(list, 3)))
-  print()
+    # permet l'affichage en ligne + le mélange avec la fonction random de la liste ci dessus.
+    print('\n'.join(random.sample(list1, 4)))
+    print()
+    print('\n'.join(random.sample(list1, 4)))
+    print()
+    print('\n'.join(random.sample(list1, 3)))
+    print()
+    print('\n'.join(random.sample(list1, 3)))
